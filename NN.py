f1a21f2b662a0bde3bb51c242c8790de146326d5
Lin Wang <orchestor@gmail.com>
2019-05-16 18:24:41 -0700

    add sigmoid and fully connnected layer

---
+++ NN.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class Sigmod:
+
+    def __init__(self):
+        self.params = []
+
+    def forward(self, x):
+        return 1 / (1 + np.exp(-x))
+
+
+class FullyConnectLayer:
+
+    def __init__(self, weight, bias):
+        self.params = [weight, bias]
+
+    def forward(self, x):
+        weight, bias = self.params
+        return np.dot(x, weight) + bias
