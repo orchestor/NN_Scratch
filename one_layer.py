d2f85b4a5629b3f648dcabdae9de9da9092a7e1a
Lin Wang <orchestor@gmail.com>
2019-06-26 12:46:06 -0700

    add one more layer and activation function

--- one_layer.py
+++ one_layer.py
@@ -1,7 +1,16 @@
 import numpy as np
 
+def sigmod(x):
+    return 1 / (1+np.exp(x))
+
 input = np.random.randn(10, 2)    # 10 samples and each sample has two features
 w1 = np.random.randn(2,4)         # 4 judges
-bias = np.random.randn(4)
-output = np.dot(input,w1) + bias
+b1 = np.random.randn(4)
+h = np.dot(input,w1) + b1
+
+w2 = np.random.randn(4,3)
+b2 = np.random.randn(3)
+
+a = sigmod(h)
+output = np.dot(a, w2) + b2
 print(output)
