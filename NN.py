7c04204d69c6cfa621082717def5a3e18120c1ff
Lin Wang <orchestor@gmail.com>
2019-07-05 17:01:47 -0700

    delete double sigmoid function

--- NN.py
+++ NN.py
@@ -1,14 +1,6 @@
 import numpy as np
 
 
-class Sigmod:
-    def __init__(self):
-        self.params = []
-
-    def forward(self, x):
-        return 1 / (1 + np.exp(-x))
-
-
 class FullyConnectLayer:
     def __init__(self, weight, bias):
         self.params = [weight, bias]
